eb4581af797c230c158173dd3f8cda10bda73f54
GK <gabykassab0@gmail.com>
2025-01-28 22:37:09 +0200

    adding import statments

--- pull/app/Console/Commands/FetchPullRequests.php
+++ pull/app/Console/Commands/FetchPullRequests.php
@@ -3,28 +3,11 @@
 namespace App\Console\Commands;
 
 use Illuminate\Console\Command;
+use Illuminate\Support\Facades\Http;
+use Illuminate\Support\Facades\Storage;
 
 class FetchPullRequests extends Command
 {
-    /**
-     * The name and signature of the console command.
-     *
-     * @var string
-     */
-    protected $signature = 'app:fetch-pull-requests';
 
-    /**
-     * The console command description.
-     *
-     * @var string
-     */
-    protected $description = 'Command description';
-
-    /**
-     * Execute the console command.
-     */
-    public function handle()
-    {
-        //
-    }
+    
 }
