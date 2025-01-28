6b51a3343dca2c77cfc8a4beddcd5c87d2c1face
GK <gabykassab0@gmail.com>
2025-01-28 22:56:31 +0200

    creating array for different search queries

--- pull/app/Console/Commands/FetchPullRequests.php
+++ pull/app/Console/Commands/FetchPullRequests.php
@@ -13,6 +13,14 @@ class FetchPullRequests extends Command
     protected $description = 'fetch pull requests from github and save results into txt files';
 
     public function handle(){
-        
+        $repository = 'woocommerce/woocommerce';
+        $baseUrl = "https://api.github.com/search/issues";
+
+        $queries = [
+            '14-days-old'=>"repo:$repository is:pr is:open created:<=14-days",
+            "review-required"=>"repo:$reposiroty is:pr is:open review:required",
+            "status-success"=>"repo:$repository is:pr is:open status:success",
+            "no-reviews" =>"repo:$repository is:pr is:open review:none",
+        ];
     }
 }
