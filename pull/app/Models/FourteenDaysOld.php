da24b461b1f1096c3eda44a592030f701053c035
GK <gabykassab0@gmail.com>
2025-01-30 22:18:32 +0200

    creating model for 14 days

--- pull/app/Models/FourteenDaysOld.php
+++ pull/app/Models/FourteenDaysOld.php
@@ -6,5 +6,6 @@ use Illuminate\Database\Eloquent\Model;
 
 class FourteenDaysOld extends Model
 {
-    //
+    protected $table = 'fourteen_days_old';
+    protected $fillable = ['pr_number', 'title', 'url'];
 }
